730fd881e6059e6ab186d9f072607a7a5da9bd38
Shipeng Feng <fsp261@gmail.com>
2014-06-27 12:22:39 +0800

    Added chapter04

---
+++ chapter04/Lambda.hs
@@ -0,0 +1,6 @@
+-- chapter04/Lambda.hs
+
+safeHead (x:_) = Just x
+safeHead _ = Nothing
+
+unsafeHead = \(x:_) -> x
---
+++ chapter04/dlts.hs
@@ -0,0 +1,11 @@
+-- chapter04/dlts.hs
+
+import Data.List (isPrefixOf)
+
+dlts :: String -> [String]
+
+dlts = foldr step [] . lines
+  where step l ds
+          | "#define DLT_" `isPrefixOf` l = secondWord l : ds
+          | otherwise                     = ds
+        secondWord = head . tail . words
