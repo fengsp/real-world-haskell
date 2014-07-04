8eb952c4fb9df05e521c4cbfdb5e189ba63ee0b1
Shipeng Feng <fsp261@gmail.com>
2014-07-04 16:07:02 +0800

    Added chapter09

---
+++ chapter09/SimpleFinder.hs
@@ -0,0 +1,8 @@
+-- chapter09/SimpleFinder.hs
+import RecursiveContents (getRecursiveContents)
+
+simpleFind :: (FilePath -> Bool) -> FilePath -> IO [FilePath]
+
+simpleFind p path = do
+  names <- getRecursiveContents path
+  return (filter p names)
---
+++ chapter09/Style.hs
@@ -0,0 +1,52 @@
+-- chapter09/Style.hs
+
+tidyLet = let foo = undefined
+              bar = foo * 2
+          in undefined
+
+weirdLet = let foo = undefined
+               bar = foo * 2
+    in undefined
+
+strangeLet = let foo = undefined
+                 bar = foo * 2 in
+    undefined
+
+commonDo, rareDo :: Monad m => m ()
+
+commonDo = do
+  something <- undefined
+  return ()
+
+-- not seen very often
+rareDo =
+  do something <- undefined
+     return ()
+
+unusualPunctuation =
+    [ (x,y) | x <- [1..a], y <- [1..b] ] where {
+                                           b = 7;
+ a = 6 }
+
+preferredLayout = [ (x,y) | x <- [1..a], y <- [1..b] ]
+    where b = 7
+          a = 6
+
+normalIndent =
+    undefined
+
+strangeIndent =
+                           undefined
+
+goodWhere = take 5 lambdas
+    where lambdas = []
+
+alsoGood =
+    take 5 lambdas
+  where
+    lambdas = []
+
+badWhere =           -- legal, but ugly and hard to read
+    take 5 lambdas
+    where
+    lambdas = []
